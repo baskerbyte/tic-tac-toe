94564263ace78f55f7dc969cd394991afe9e09f6
Luis <baskerbyte@gmail.com>
2024-05-23 05:38:26 -0300

    feat: handle player disconnect

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -5,15 +5,24 @@
 
     let playerName = '';
     let opponentName = '';
+    let opponentLeft: boolean | null = null;
+    let id: number | null = null;
     let my_turn: boolean | null = null;
     let isFormSubmitted = false;
     let socket: WebSocket | null = null;
+    let match_result: number | null;
 
     onMount(() => {
+        connect()
+    });
+
+    function connect() {
         socket = new WebSocket("ws://localhost:9002");
 
         socket.addEventListener('open', () => {
-            console.log('WebSocket connection opened');
+            if (playerName) {
+                playAgain();
+            }
         });
 
         socket.addEventListener('message', (event) => {
@@ -23,20 +32,50 @@
             if (data.opcode == 13) {
                 opponentName = data.d.name;
                 my_turn = data.d.id != 0;
+                if (data.d.id == 0) {
+                    id = 1;
+                } else {
+                    id = 0;
+                }
+            } else if (data.opcode == 11) {
+                match_result = data.d.status;
+                isFormSubmitted = false;
+            } else if (data.opcode == 14) {
+                opponentName = '';
+                isFormSubmitted = false;
+                opponentLeft = true;
             }
         });
 
         socket.addEventListener('close', () => {
-            console.log('WebSocket connection closed');
+            socket = null;
+            opponentName = '';
         });
 
         socket.addEventListener('error', () => {
             console.error('WebSocket error');
         });
-    });
+    }
 
     const handleSubmit: EventHandler<Event, HTMLFormElement> = async (event) => {
         event.preventDefault();
+
+        if (socket) {
+            isFormSubmitted = true;
+            socket.send(JSON.stringify({
+                opcode: 12,
+                d: {
+                    name: playerName
+                }
+            }))
+        }
+    }
+
+    function playAgain() {
+        opponentName = '';
+
+        id = null;
+        my_turn = null;
 
         if (socket) {
             isFormSubmitted = true;
@@ -59,9 +98,22 @@
             <p>Finding a match</p>
         {/if}
     {:else}
-        <form on:submit|preventDefault={handleSubmit}>
-            <input type="text" bind:value={playerName} placeholder="Enter your name"/>
-            <button type="submit">Find Match</button>
-        </form>
+        {#if !socket}
+            <button on:click={connect}>Reconnect</button>
+        {:else if match_result}
+            <p>{match_result === 1 ? (id === 0 ? "You won" : `${opponentName} won`) : "Tie!"}</p>
+
+            <button on:click={playAgain}>Play Again</button>
+        {:else}
+            {#if opponentLeft}
+                <p>Opponent left the match</p>
+                <button on:click={playAgain}>Play Again</button>
+            {:else}
+                <form on:submit|preventDefault={handleSubmit}>
+                    <input type="text" bind:value={playerName} placeholder="Enter your name"/>
+                    <button type="submit">Find Match</button>
+                </form>
+            {/if}
+        {/if}
     {/if}
 </main>
